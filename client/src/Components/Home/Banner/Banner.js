6561529fdcb3940e45fc9c91ffdbbef77935dead
Shakib448 <shakiba448@gmail.com>
2020-11-18 17:08:39 +0600

    Banner animation added

--- client/src/Components/Home/Banner/Banner.js
+++ client/src/Components/Home/Banner/Banner.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Banner.sass";
+import ReactTypingEffect from "react-typing-effect";
 
 const Banner = () => {
   return (
     <section className="banner">
       <h1>Welcome Student</h1>
+      <h2>
+        <ReactTypingEffect
+          speed={100}
+          text={["I hope the requirements is done!"]}
+        />
+      </h2>
     </section>
   );
 };
