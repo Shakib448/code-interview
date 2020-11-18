53f730421414cc993178ce945664a630e6333add
Shakib448 <shakiba448@gmail.com>
2020-11-18 10:28:17 +0600

    sidebard added

--- client/src/Components/Dashboard/Sidebar/Sidebar.js
+++ client/src/Components/Dashboard/Sidebar/Sidebar.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import ListIcon from "@material-ui/icons/List";
 
 const Sidebar = ({ heading }) => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">{heading}</Navbar.Brand>
+          <Navbar.Brand href="#home">Image-upload </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
               <NavDropdown title={heading} id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Upload</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">
+                  <CloudUploadIcon /> Upload
+                </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
-                  Upload List
+                  <ListIcon /> Upload List
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
