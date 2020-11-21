ccf5b9ae93599e65d68c6dbf1cd3464d26d04aaa
Shakib448 <shakiba448@gmail.com>
2020-11-21 07:16:00 +0600

    Loading method added

--- client/src/Components/Home/UploadResult/UploadResult.js
+++ client/src/Components/Home/UploadResult/UploadResult.js
@@ -4,10 +4,13 @@ import "./UploadResult.sass";
 import HomeNav from "../HomeNav/HomeNav";
 import { userInformationData } from "../../../App";
 import AxiosConfig from "../../AxiosConfig/AxiosConfig";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const UploadResult = () => {
   const [studentTask, setStudentTask] = useState([]);
   const [userData, setUserData] = useContext(userInformationData);
+
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const filterData = async () => {
@@ -16,6 +19,7 @@ const UploadResult = () => {
           "/studentTask?email=" + userData.email
         );
         setStudentTask(res.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -28,42 +32,51 @@ const UploadResult = () => {
       <HomeNav />
 
       <Container>
-        <Row>
-          {studentTask.map((stk) => (
-            <Col key={stk._id} lg={4} md={6} sm={12}>
-              <Card className="uploadTask__card">
-                <Card.Img
-                  variant="top"
-                  src={`data:image/jpeg;base64,${stk.image.img}`}
-                />
-                <Card.Body>
-                  <Card.Title>{stk.task}</Card.Title>
-                  <Accordion defaultActiveKey="0">
-                    <Card className="uploadTaskCollapse__card">
-                      <Card.Header className="text-center">
-                        <Accordion.Toggle
-                          className="uploadTask__btn"
-                          variant="success"
-                          as={Button}
-                          eventKey="1"
-                        >
-                          Show Result
-                        </Accordion.Toggle>
-                      </Card.Header>
-                      <Accordion.Collapse eventKey="1">
-                        <Card.Body className="text-center">
-                          <Button className="uploadTask__btn" variant="success">
-                            {stk.marks}
-                          </Button>
-                        </Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                  </Accordion>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+        {!loading ? (
+          <Row>
+            {studentTask.map((stk) => (
+              <Col key={stk._id} lg={4} md={6} sm={12}>
+                <Card className="uploadTask__card">
+                  <Card.Img
+                    variant="top"
+                    src={`data:image/jpeg;base64,${stk.image.img}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{stk.task}</Card.Title>
+                    <Accordion defaultActiveKey="0">
+                      <Card className="uploadTaskCollapse__card">
+                        <Card.Header className="text-center">
+                          <Accordion.Toggle
+                            className="uploadTask__btn"
+                            variant="success"
+                            as={Button}
+                            eventKey="1"
+                          >
+                            Show Result
+                          </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                          <Card.Body className="text-center">
+                            <Button
+                              className="uploadTask__btn"
+                              variant="success"
+                            >
+                              {stk.marks}
+                            </Button>
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                    </Accordion>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Row className="justify-content-center">
+            <CircularProgress color="secondary" />
+          </Row>
+        )}
       </Container>
     </>
   );
